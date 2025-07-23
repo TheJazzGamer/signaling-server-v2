8016d96555d6cae21670fd39879c7b7e1272d5c4
TheJazzGamer <117808246+TheJazzGamer@users.noreply.github.com>
2025-07-23 11:11:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -80,9 +80,9 @@ wss.on("connection", (socket) => {
 
 console.log("Signaling server running...");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 if (!process.env.PORT) {
-    console.warn("⚠️ No process.env.PORT — using fallback port", port);
+    console.warn("⚠️ No process.env.PORT);
 }
 server.listen(port, '0.0.0.0', () => {
     console.log(`✅ HTTP + WebSocket server running on port ${port}`);
